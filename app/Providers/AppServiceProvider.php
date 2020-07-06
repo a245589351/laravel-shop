3f2f338be10407df2ca4cd5ffa7be518958bd55c
ve.chen <weiyi963@qq.com>
2020-07-06 08:49:07 +0000

    订单支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
+            $config['notify_url'] = 'http://requestbin.net/r/s42kivs4';
+            $config['notify_url'] = route('payment.alipay.notify');
+            $config['return_url'] = route('payment.alipay.return');
+
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
                 $config['mode']         = 'dev';
@@ -25,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
             } else {
                 $config['log']['level'] = Logger::WARNING;
             }
+
             // 调用 Yansongda\Pay 来创建一个支付宝支付对象
             return Pay::alipay($config);
         });
