905a1c73bd508f2f4474500ebbea31d3a2669ba8
ve.chen <weiyi963@qq.com>
2020-07-06 06:50:09 +0000

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,15 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
+    // 产品相关
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
+    // 购物车相关
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
